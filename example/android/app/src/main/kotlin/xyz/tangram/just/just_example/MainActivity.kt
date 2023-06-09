3c228fce496abd2b2bcc2e57a660f21c831c893f
linyongsheng2 <linyongsheng2@joyy.com>
2023-06-09 10:59:04 +0800

    init

---
+++ example/android/app/src/main/kotlin/xyz/tangram/just/just_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package xyz.tangram.just.just_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/just.dart
@@ -0,0 +1,3 @@
+String test() {
+  return "hello";
+}
